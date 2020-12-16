54f1a55b8d6dfcf93d49668ceaebe8920a9693c2
chobi1125 <chonmage112000@yahoo.co.jp>
2020-12-16 22:58:35 +0900

    双方向バインディングとパイプ

--- src/app/sample.component.ts
+++ src/app/sample.component.ts
@@ -11,11 +11,17 @@ import { Component } from '@angular/core';
       <input #txt id="txt" name="txt" type="text" (keyup.enter)="input(txt.value)" />
       <ul [innerHTML]="msg"></ul>
     </div>
+    <form>
+      <label for="name">名前：</label>
+      <input id="name" name="name" type="text" [(ngModel)]="myName" autocomplete="off" />
+      <p>こんにちは、{{myName | lowercase}}さん！！</p>
+    </form>
   `,
 })
 export class SampleComponent  {
   time: string = '---';
   msg: string = "";
+  myName: string = "佐藤";
   // テンプレートステートメントで$eventとすることでイベント情報を取得できる
   show(e: any) {
     this.time = new Date().toLocaleString();
