8af84545326bc51c5a295ad8b17a3524ca7a71ea
chobi1125 <chonmage112000@yahoo.co.jp>
2020-12-16 22:51:13 +0900

    keyup.enterによる片方向バインディングまで

--- src/app/sample.component.ts
+++ src/app/sample.component.ts
@@ -2,6 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sample-app',
-  template: `<h1>Sample {{name}}</h1>`,
+  template: `
+    <div>
+      <input type="button" (click)="show($event)" value="現在時刻" />
+      <p>{{time}}</p>
+    </div>
+    <div>
+      <input #txt id="txt" name="txt" type="text" (keyup.enter)="input(txt.value)" />
+      <ul [innerHTML]="msg"></ul>
+    </div>
+  `,
 })
-export class SampleComponent  { name = 'World'; }
+export class SampleComponent  {
+  time: string = '---';
+  msg: string = "";
+  // テンプレートステートメントで$eventとすることでイベント情報を取得できる
+  show(e: any) {
+    this.time = new Date().toLocaleString();
+    console.log(e)
+  }
+  // テンプレート参照変数※keyup.enterでエンター時のみ入力値を反映
+  input(input: string){
+    this.msg += `<li>${input}</li>`;
+  }
+}
